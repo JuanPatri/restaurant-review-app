37a6953635d6d0c7186da15590e04df412e347f8
JuanPatri <JP266824@fi365.ort.edu.uy>
2025-07-19 00:22:05 -0300

    create favorite entity

--- Domain/Favorite.cs
+++ Domain/Favorite.cs
@@ -2,5 +2,35 @@
 
 public class Favorite
 {
+    private int _id;
+    private User _user;
+    private Restaurant _restaurant;
+    private DateTime _date;
+
+
+    public int Id { get => _id; set => _id = value; }
+
+    public User User
+    {
+        get => _user;
+        set => _user = value ?? throw new ArgumentNullException(nameof(value), "User cannot be null.");
+    }
+
+    public Restaurant Restaurant
+    {
+        get => _restaurant;
+        set => _restaurant = value ?? throw new ArgumentNullException(nameof(value), "Restaurant cannot be null.");
+    }
+
+    public DateTime Date
+    {
+        get => _date;
+        set
+        {
+            if (value == default)
+                throw new ArgumentException("Date must be a valid date.", nameof(value));
+            _date = value;
+        }
+    }
     
 }
